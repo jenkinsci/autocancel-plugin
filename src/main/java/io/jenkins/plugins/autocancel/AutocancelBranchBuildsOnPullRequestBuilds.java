2d7b76b956b2cd81923535e78b821dd4801e21a3
David Recuenco <david.recuenco@adidas-group.com>
2020-06-26 11:12:41 +0200

    FIX string comparison with .equals()

--- src/main/java/io/jenkins/plugins/autocancel/AutocancelBranchBuildsOnPullRequestBuilds.java
+++ src/main/java/io/jenkins/plugins/autocancel/AutocancelBranchBuildsOnPullRequestBuilds.java
@@ -1,11 +1,15 @@
 package io.jenkins.plugins.autocancel;
 
+import java.io.PrintStream;
+import java.net.URI;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Set;
 
+import hudson.console.ModelHyperlinkNote;
 import hudson.model.*;
 import io.jenkins.plugins.autocancel.interruption.SupersededInterruption;
+import jenkins.model.Jenkins;
 import org.jenkinsci.plugins.workflow.steps.*;
 import org.kohsuke.stapler.DataBoundConstructor;
 
@@ -33,25 +37,41 @@ public class AutocancelBranchBuildsOnPullRequestBuilds extends Step {
         protected Void run() throws Exception {
             Run currentBuild = getContext().get(Run.class);
             TaskListener task = getContext().get(TaskListener.class);
+
+            PrintStream logger = task.getLogger();
             String changeBranch = currentBuild.getEnvironment(task).get("CHANGE_BRANCH", null);
 
             if (changeBranch == null) {
                 return null;
             }
+            logger.println(String.format("Current change branch: %s", changeBranch));
 
+            URI jenkinsUri = new URI(Jenkins.get().getRootUrl());
             int currentBuildNumber = currentBuild.getNumber();
             Job pullRequestJob = currentBuild.getParent();
             Collection<Job> jobs = pullRequestJob.getParent().getAllItems(Job.class);
 
             for (Job job : jobs) {
-                if (changeBranch == job.getDisplayName()) {
+                if (changeBranch.equals(job.getDisplayName())) {
+                    final String jobUrl = jenkinsUri.resolve(job.getUrl()).toString();
+
+                    logger.println(String.format("Found matching job: %s", ModelHyperlinkNote.encodeTo(jobUrl, job.getDisplayName())));
+
                     Collection<Run> builds = job.getBuildsAsMap().values();
 
                     for (Run build : builds) {
                         if (build.isBuilding()) {
                             final String message = String.format(MESSAGE_TEMPLATE, build.number, pullRequestJob.getName(), currentBuildNumber);
+                            final String buildUrl = jenkinsUri.resolve(build.getUrl()).toString();
 
-                            build.getExecutor().interrupt(Result.ABORTED, new SupersededInterruption(message));
+                            logger.println(String.format("Stopping branch job %s", ModelHyperlinkNote.encodeTo(buildUrl, build.getDisplayName())));
+
+                            try {
+                                build.getExecutor().interrupt(Result.ABORTED, new SupersededInterruption(message));
+                            } catch (Exception e) {
+                                logger.println(String.format("Failed to stop job %s", ModelHyperlinkNote.encodeTo(buildUrl, build.getDisplayName())));
+                                logger.println(e.getMessage());
+                            }
                             build.setDisplayName(message);
                         }
                     }
