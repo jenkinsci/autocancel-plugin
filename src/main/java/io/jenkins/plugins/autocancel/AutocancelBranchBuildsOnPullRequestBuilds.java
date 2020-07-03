3306add6f14cd5496f32afc3b855e70d4b918ec9
David Recuenco <david.recuenco@adidas-group.com>
2020-07-03 08:43:02 +0200

    FIX bugs found by spotbugs

--- src/main/java/io/jenkins/plugins/autocancel/AutocancelBranchBuildsOnPullRequestBuilds.java
+++ src/main/java/io/jenkins/plugins/autocancel/AutocancelBranchBuildsOnPullRequestBuilds.java
@@ -68,7 +68,10 @@ public class AutocancelBranchBuildsOnPullRequestBuilds extends Step {
                             logger.println(String.format("Stopping branch job %s", ModelHyperlinkNote.encodeTo(buildUrl, build.getDisplayName())));
 
                             try {
-                                build.getExecutor().interrupt(Result.ABORTED, new SupersededInterruption(message));
+                                Executor executor = build.getExecutor();
+                                if (executor != null) {
+                                    executor.interrupt(Result.ABORTED, new SupersededInterruption(message));
+                                }
                                 build.doTerm();
                             } catch (Exception e) {
                                 logger.println(String.format("Failed to stop job %s", ModelHyperlinkNote.encodeTo(buildUrl, build.getDisplayName())));
